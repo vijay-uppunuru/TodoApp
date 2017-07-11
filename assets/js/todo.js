01e215eef2b761e9e9c8c5684c7b621167aaf89e
vijay-uppunuru <uvijaysimha@gmail.com>
2017-07-11 23:19:32 +0530

    first time

---
+++ assets/js/todo.js
@@ -0,0 +1,29 @@
+$("ul").on("click", "li" ,function(){
+	$(this).toggleClass("completed");
+	
+});
+
+
+$("ul").on("click","span",function(event){
+
+	$(this).parent().fadeOut(500,function(){
+
+		$(this).remove();
+	});
+	
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todotxt = $(this).val();
+		$(this).val("");
+		$("ul").prepend("<li><span> <i class='fa fa-trash-o'></i> </span>"+todotxt+"</li>");
+
+	}
+});
+
+
+$(".fa-pencil").click(function(){
+	$("input[type='text']").fadeToggle(); 
+});
